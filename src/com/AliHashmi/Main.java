6d2a072d71abf241349421bb466902d15840e082
alihashmi94 <me.alihashmi@gmail.com>
2022-11-19 16:46:31 +0500

    Solar System Basic Specifications added. Some features still under development

---
+++ src/com/AliHashmi/Main.java
@@ -0,0 +1,8 @@
+package com.AliHashmi;
+
+public class Main {
+
+    public static void main(String[] args) {
+	SolarSystem milkyWay = new SolarSystem();
+    }
+}
---
+++ src/com/AliHashmi/SolarSystem.java
@@ -0,0 +1,8 @@
+package com.AliHashmi;
+
+public class SolarSystem {
+    int sun;
+    int moon;
+    int planets;
+
+}
