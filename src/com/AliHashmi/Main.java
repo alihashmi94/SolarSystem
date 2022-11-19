1477c7bab102f00f77ad5ad2792a160f236e5d80
alihashmi94 <me.alihashmi@gmail.com>
2022-11-19 17:03:14 +0500

    The Basic Structure is Complete

--- src/com/AliHashmi/Main.java
+++ src/com/AliHashmi/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	SolarSystem milkyWay = new SolarSystem();
+    milkyWay.sun = 1;
+    milkyWay.moon = 2;
+    milkyWay.planets = 3;
+    
     }
 }
